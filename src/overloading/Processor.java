b4ee97cababd85ddc3708762dc2bfc360d47232a
ACER <ACER@NhatTuyen>
2024-09-06 11:32:15 +0700

    Nhattuyen

---
+++ src/overloading/Processor.java
@@ -0,0 +1,11 @@
+
+package overloading;
+
+public class Processor {
+    public static void main(String[] args) {
+        Overloading object = new Overloading();
+        System.out.println(object.sum(1, 2));
+        System.out.println(object.sum(1,2,3));
+    }
+    
+}
